c69432289f763b6507a8c3cb49a7b588e13914f4
luciferDc <942316156@qq.com>
2020-10-13 09:35:59 +0800

    consumer start

--- service/queue/consumerService.js
+++ service/queue/consumerService.js
@@ -1,5 +1,6 @@
 const BaseService = require('../service.base')
 const System = require('../../utils/system')
+const axios = require('axios')
 class ConsumerService extends BaseService {
   constructor() {
     super()
@@ -13,13 +14,48 @@ class ConsumerService extends BaseService {
       let id = popData[0].value[0].id
       let data = popData[0].value[0].data
       
+      let requestConfig = this.getRequestConfig(data)
+      let res = await axios.request(requestConfig)
+      // TODO: 记录日志
+      if (res.code == 0) {
+        await this.redisClient.xack(this.config.queueName, this.config.queueGroup, id)
+      } 
     } catch (error) {
-      
+      // TODO: 记录日志
     }
+    this.doConsumer()
   }
 
   async doPendingConsumer() {
+    try {
+      
+    } catch (error) {
+      
+    }
 
+    setTimeout(async () => {
+      await this.doPendingConsumer()
+    }, 5000);
+  }
+
+  getRequestConfig(data) {
+    let typeBody = ['PUT', 'POST', 'PATCH']
+    let httpConfig = {
+      url: data.url
+    }
+
+    if(data.param) {
+      if (data.method && typeBody.includes(data.method)) {
+        httpConfig.method = data.method
+        httpConfig.data = data.param
+      } else {
+        httpConfig.param = data.param
+      }
+    }
+
+    if (data.header) {
+      httpConfig.headers = data.header
+    }
   }
 }
 module.exports = ConsumerService
